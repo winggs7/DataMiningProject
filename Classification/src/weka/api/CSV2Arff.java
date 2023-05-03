63acc3e8bfe897611c1515e2c51cccf3435ab7d3
Danh <danhnguyen17047@gmail.com>
2023-05-03 23:33:18 +0700

    refactor CSV2Arff java code

--- Classification/src/weka/api/CSV2Arff.java
+++ Classification/src/weka/api/CSV2Arff.java
@@ -10,14 +10,19 @@ public class CSV2Arff {
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+		String filename = "Test_set";
 		CSVLoader loader = new CSVLoader();
-		loader.setSource(new File("4_Cate_SMOTE.csv"));
+		loader.setSource(new File(filename + ".csv"));
+		String[] nominalVals = {"Sex,Category:nominal,nominal"};
+		loader.setNominalLabelSpecs(nominalVals);
+		
 		Instances data = loader.getDataSet();
+		data.setClass(data.attribute("Category"));
 		
 		ArffSaver saver = new ArffSaver();
 		saver.setInstances(data);
 		
-		saver.setFile(new File("4_Cate_SMOTE.arff"));
+		saver.setFile(new File(filename + ".arff"));
 		saver.writeBatch();
 	}
 
